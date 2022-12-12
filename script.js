a890e8d89600c586f81508a275ecfbd9f19ab521
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2022-12-12 16:48:03 -0300

    Cambios en Index y Script
    Cambios en Index.
    
    Se agrega formato de Hexadecimal y RGB
    
    Se termina script de Hexadecimal, RGB y visualizar color por el fondo.

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-function ColorAleatori(){
+function run(){
     let simbolos = '0123456789abcdef'
     let color = '#'
 
     for(let i = 0; i < 6; i++)
         color += simbolos[Math.floor(Math.random() * 16)]
-    
+      
+    const hexArgb = (color) => {
+        const r = parseInt(color.slice(1, 3), 16);
+        const g = parseInt(color.slice(3, 5), 16);
+        const b = parseInt(color.slice(5, 7), 16);
+        return [r, g, g];
+    }
+
     document.body.style.background = color
+    let texto = document.getElementById("hex")
+    texto.innerHTML = color
+
+    document.getElementById("rgb").innerHTML = "rgb(" + hexArgb(color) + ")";
 }
