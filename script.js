55ace5b9dc12e1a80d5bbe2d7ba72fcea81f0d12
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2022-12-12 15:38:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function ColorAleatori(){
     let simbolos = '0123456789abcdef'
     let color = '#'
+
+    for(let i = 0; i < 6; i++)
+        color += simbolos[Math.floor(Math.random() * 16)]
+    
+    document.body.style.background = color
 }
