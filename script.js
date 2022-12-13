cc3f8c560c7e83806c035b320a5d44b7e326f228
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2022-12-13 14:55:20 -0300

    Agregado Media Query

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function run(){
         const r = parseInt(color.slice(1, 3), 16);
         const g = parseInt(color.slice(3, 5), 16);
         const b = parseInt(color.slice(5, 7), 16);
-        return [r, g, g];
+        return [r, g, b];
     }
 
     document.body.style.background = color
